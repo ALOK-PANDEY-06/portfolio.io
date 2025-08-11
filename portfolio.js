441aeff4df9e23c31730c25ae1001a93a2d854e8
Alok Pandey <al0kpandey6405@gmail.com>
2025-08-11 17:46:54 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+// Typing Effect
+const textArray = ["Web Developer", "UI/UX Designer", "Creative Coder"];
+let typingText = document.querySelector(".typing-text");
+let arrayIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+    if (charIndex < textArray[arrayIndex].length) {
+        typingText.textContent += textArray[arrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else {
+        setTimeout(eraseEffect, 2000);
+    }
+}
+
+function eraseEffect() {
+    if (charIndex > 0) {
+        typingText.textContent = textArray[arrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseEffect, 50);
+    } else {
+        arrayIndex = (arrayIndex + 1) % textArray.length;
+        setTimeout(typeEffect, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    typeEffect();
+});
